e4f1ddf5a9542a87d4a2995d1d246c2c9d4896bb
Gavin-1001 <gavinshelley10@gmail.com>
2023-11-05 20:51:27 +0000

    Removed Example test code

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -102,17 +102,3 @@ test("Update User", async({request}) =>{
 
 })
 
-
-
-
-
-/*test('POST user Login', async ({ request }) => {
-await page.goto('https://playwright.dev/');
-
-// Click the get started link.
-await page.getByRole('link', { name: 'Get started' }).click();
-
-// Expects page to have a heading with the name of Installation.
-await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});*/
-
